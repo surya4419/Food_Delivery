774e76e301c940a98eab1d0457a808ccc292d55d
suryaa4419@gmail.com <surya4419@gmail.com>
2024-06-24 23:27:26 +0530

    user login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,53 @@
- 
+import userModel from "../models/usermodel.js";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import validator from "validator"
+
+
+//login user
+const loginUser = async (req,res) => {
+
+}
+
+const createToken = (id) =>{
+    return jwt.sign({id},)
+}
+
+//register user
+const registerUser = async (req,res) => {
+    const {name,password,email} = req.body;
+    try{
+        //checking if user already exists
+        const exists = await userModel.findOne({email});
+        if(exists){
+            return res.json({success:false,message:"User already exits"})
+        }
+
+        //validating email format & strong password
+        if(!validator.isEmail(email)){
+            return res.json({success:false,message:"Please enter a valid email"})
+        }
+
+        if (password.length<8) {
+            return res.json({success:false,message:"Please enter a strong Password"})
+        }
+
+        //hashing user password 
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save()
+
+    }catch(error){
+
+    }
+
+}
+
+export { loginUser ,registerUser}
