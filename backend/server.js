37ea81ecadda6cbdd11c2db144dac01415f9b9c8
suryaa4419@gmail.com <surya4419@gmail.com>
2024-08-18 23:06:18 +0530

    added port

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import orderRouter from "./routes/orderRouter.js"
 
 //app config
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 //middleware
 app.use(express.json())
