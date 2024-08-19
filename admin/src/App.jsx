92472b65a4f8a9b37e822020d40cfc02b034773a
suryaa4419@gmail.com <surya4419@gmail.com>
2024-08-19 11:47:06 +0530

    updated  backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-    const url = "http://localhost:4000"
+    const url = "https://pumatobackend.onrender.com"
 
   return (
     <div>
