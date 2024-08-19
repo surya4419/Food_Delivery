1df080f9dc40890b1105e0225a493ffc6faef943
suryaa4419@gmail.com <surya4419@gmail.com>
2024-08-19 12:28:24 +0530

    updated frontend url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order from frontend
 const placeOrder = async (req,res) =>{
 
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://pumato.onrender.com"
 
     try{
         const newOrder = new orderModel({
